7d66873f725fcd2d5e8086d7a84366f20885097d
edalsare <easrevelo@gmail.com>
2022-12-02 12:28:00 -0500

    añadiendo condicionales sobrepeso js

--- js/index.js
+++ js/index.js
@@ -8,6 +8,19 @@ function calcular(){
     var peso = document.getElementById("peso").value;
     var BMI = peso/(Math.pow(talla, 2));
     BMI = BMI.toFixed(2);
-    document.getElementById("masa").value = BMI;
+    
+
+    if(BMI>= 18.5 && BMI<=24.9){
+        document.getElementById("masa").value = BMI + " NORMAL";
+    } else if (BMI>= 25 && BMI<=29.9) {
+        document.getElementById("masa").value = BMI + " SOBREPESO";
+    }else if (BMI>= 30 && BMI<=34.9) {
+        document.getElementById("masa").value = BMI + " OBESIDAD GRADO 1";
+    }else if (BMI>= 35 && BMI<=39.9) {
+        document.getElementById("masa").value = BMI + " OBESIDAD GRADO 2";
+    }else if (BMI>= 40) {
+        document.getElementById("masa").value = BMI + " OBESIDAD GRADO 3";
+    }
+    
 
 }
